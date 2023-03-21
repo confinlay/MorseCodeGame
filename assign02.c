d5bd53e8ba4ff63a9f9cf4a5d34d1ef1a8233a78
confinlay <conorfin02@gmail.com>
2023-03-21 12:01:53 +0000

    LEDs work in progress

--- assign02.c
+++ assign02.c
@@ -1,10 +1,25 @@
-// Must declare the main assembly entry point before use.
-void main_asm();
+#include "pico/stdlib.h"
 
-/*
- * Main entry point for the code - simply calls the main assembly function.
- */
+#define LED_PIN 25
+
+#define RED_PIN 0
+#define GREEN_PIN 1
+#define BLUE_PIN 2
+
 int main() {
-    main_asm();
-    return(0);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 0);
+
+    gpio_init(RED_PIN);
+    gpio_set_dir(RED_PIN, GPIO_OUT);
+    gpio_put(RED_PIN, 1);
+    
+    while (true) {
+        gpio_put(LED_PIN, 1);   // turn the LED on
+        sleep_ms(1000);         // wait for 1 second
+        gpio_put(LED_PIN, 0);   // turn the LED off
+        sleep_ms(1000);         // wait for 1 second
+    }
+    
 }
