920bba24d4851022955c3d20d8dadcb9e60d76a8
fos9191 <125087856+fos9191@users.noreply.github.com>
2023-03-21 00:24:05 +0000

    gpio interrupt setup, falling and rising edge

--- assign02.c
+++ assign02.c
@@ -1,10 +1,43 @@
-// Must declare the main assembly entry point before use.
-void main_asm();
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
-/*
- * Main entry point for the code - simply calls the main assembly function.
- */
+// Declare the main assembly code entry point. //
+void main_asm();
+// Initialise a GPIO pin – see SDK for detail on gpio_init()
+void asm_gpio_init(uint pin) {
+ gpio_init(pin);
+}
+// Set direction of a GPIO pin – see SDK for detail on gpio_set_dir()
+void asm_gpio_set_dir(uint pin, bool out) {
+ gpio_set_dir(pin, out);
+}
+// Get the value of a GPIO pin – see SDK for detail on gpio_get()
+bool asm_gpio_get(uint pin) {
+ return gpio_get(pin);
+}
+// Set the value of a GPIO pin – see SDK for detail on gpio_put()
+void asm_gpio_put(uint pin, bool value) {
+ gpio_put(pin, value);
+}
+
+// Enable falling-edge interrupt – see SDK for detail on gpio_set_irq_enabled()
+void asm_gpio_set_irq(uint pin) {
+ gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_FALL, true);
+}
+
+// Enable rising-edge interrupt 
+void asm_gpio_set_irq1(uint pin) {
+ gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_RISE, true);
+}
+
+// Main entry point of the application
 int main() {
-    main_asm();
-    return(0);
+ stdio_init_all(); // Initialise all basic IO
+
+ printf("Welcome to our Morse Code Game!\n"); // Basic print to console
+
+
+ main_asm(); // Jump into the ASM code
+ return 0; // Application return code
 }
